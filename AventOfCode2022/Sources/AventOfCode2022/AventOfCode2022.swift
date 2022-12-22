45ed6ba2493ec169aab68272cde47ccba48a225f
CPea <vienpro2506@gmail.com>
2022-12-23 02:27:59 +0700

    update AOC 2022 commandline template

--- AventOfCode2022/Sources/AventOfCode2022/AventOfCode2022.swift
+++ AventOfCode2022/Sources/AventOfCode2022/AventOfCode2022.swift
@@ -1,35 +1,84 @@
 import ArgumentParser
+import Foundation
 
-struct Solution: ParsableCommand {
-    public static let configuration = CommandConfiguration(abstract: "Avent of Code 2022")
+protocol Avent {
+    static var day: UInt8 { get }
+    init(data: String)
+    func part1() -> UInt
+    func part2() -> UInt
+}
 
-    @Argument(help: "AOC Day")
-    private var day: Int8
+@available(macOS 13, *)
+struct Solution<Event: Avent> {
+    var event: Event?
+    var time: Int64?
+    var day: UInt8?
 
-    @Argument(help: "Uses example file provided by AOC")
-    private var example: Bool
+    init(data: String) {
+        (event, time) = getTime { Event(data: data) }
+        day = Event.day
+    }
 
-    @Argument(help: "Gets all solutions for all AOC days")
-    private var all: Bool
+    @available(macOS 13, *)
+    func getResult() {
+        let (time1, part1) = getTime(for: event!.part1)
+        let (time2, part2) = getTime(for: event!.part2)
 
-    func run() throws {}
+        print("""
+        Solution for day \(day!)
+        Collect data in \(time!)
+        part 1: \(part1) in \(time1)
+        part 2: \(part2) in \(time2)
+        """)
+    }
 
     /// Get the time to compute solution
-    @available(macOS 13, *)
-    func getTime<T>(for solution: () -> T) -> (T?, Int64) {
+    func getTime<T>(for solution: () -> T) -> (T, Int64) {
         let clock = ContinuousClock()
         var result: T?
         let time = clock.measure {
             result = solution()
         }
 
-        return (result, time.components.seconds)
+        return (result!, time.components.seconds)
+    }
+}
+
+struct AOC2022: ParsableCommand {
+    public static let configuration = CommandConfiguration(abstract: "Avent of Code 2022")
+
+    @Argument(help: "AOC Day")
+    private var day: UInt8
+
+    @Flag(name: .shortAndLong, help: "Uses example file provided by AOC")
+    private var example: Bool = false
+
+    @Flag(name: .shortAndLong, help: "Gets all solutions for all AOC days")
+    private var all: Bool = false
+
+    @available(macOS 13, *)
+    func run() throws {
+        let mainFile = example ? "example" : "input"
+        let filePath = "./day\(day)/\(mainFile).txt"
+        let url = URL(filePath: filePath)
+        let data = try! String(contentsOf: url)
+
+        var solution = Solution<Day1>(data: data)
+
+        switch day {
+        case 1:
+            solution = Solution<Day1>(data: data)
+        default:
+            print("not yet implemented")
+        }
+
+        solution.getResult()
     }
 }
 
 @main
 public enum AventOfCode2022 {
     public static func main() {
-        Solution.main()
+        AOC2022.main()
     }
 }
---
+++ AventOfCode2022/Sources/AventOfCode2022/day1/init.swift
@@ -0,0 +1,13 @@
+struct Day1: Avent {
+    static var day: UInt8 = 1
+
+    init(data _: String) {}
+
+    func part1() -> UInt {
+        0
+    }
+
+    func part2() -> UInt {
+        0
+    }
+}
